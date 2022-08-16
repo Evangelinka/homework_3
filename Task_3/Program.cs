d03558e8fbea7265e0240e9125b3c31a737140da
«Kristina» <kristinabol1996@gmail.com>
2022-08-17 00:20:33 +0800

    3 задача

---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void CubesN(int N)
+{
+    int i = 1;
+    while (i < N)
+    {
+        Console.Write($"{Math.Pow(i, 3)}, ");
+        i++;
+    }
+    Console.Write($"{Math.Pow(i, 3)}. ");
+}
+Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+CubesN(number);
